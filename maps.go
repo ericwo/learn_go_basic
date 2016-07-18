e994f581b893130ba2299a63be18e8db59fc22a9
Eric Wu <ericwu1989@gmail.com>
2016-07-18 17:39:11 +0800

    Basic map exercise

---
+++ maps.go
@@ -0,0 +1,21 @@
+// A map maps keys to values.
+// The zero value of a map is nil. A nil map has no keys, nor can keys be added.
+// The make function returns a map of the given type, initialized and ready for use.
+
+package main
+
+import "fmt"
+
+type Vertex struct {
+    Lat, Long float64
+}
+
+var m map[string]Vertex
+
+func main() {
+    m = make(map[string]Vertex)
+    m["Bell Labs"] = Vertex{
+        40.001, -74.005
+    }
+    fmt.Println(m["Bell Labs"])
+}
