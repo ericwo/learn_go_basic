88cc648640aa42f1dfae1f08ce0b72d1e35378ad
Eric Wu <ericwu1989@gmail.com>
2016-07-18 16:00:39 +0800

    range

---
+++ range-continued.go
@@ -0,0 +1,18 @@
+// You can skip the index or value by assigning to _.
+
+// If you only want the index, drop the ", value" entirely.
+package main
+
+import "fmt"
+
+func main() {
+    pow := make([]int, 10)
+
+    for i, _ := range pow {
+        pow[i] = 1 << uint(i)
+    }
+
+    for _, value := range pow {
+        fmt.Printf("%d\n", value)
+    }
+}
