11b74a133ac2717865c395e783e5f7bf8811c9a9
Eric Wu <ericwu1989@gmail.com>
2016-08-03 18:10:19 +0800

    Spider

---
+++ itunes_spider.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/jackdanger/collectlinks"
+	"net/http"
+	"os"
+)
+
+func main() {
+	flag.Parse()
+
+	args := flag.Args()
+	fmt.Println(args)
+	if len(args) < 1 {
+		fmt.Println("Please specify start page")
+		os.Exit(1)
+	}
+
+	resp, err := http.Get(args[0])
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	links := collectlinks.All(resp.Body)
+
+	for _, link := range links {
+		fmt.Println(link)
+	}
+}
