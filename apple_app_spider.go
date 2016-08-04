1a8f77436fdadaca280452a1cb1744923400e402
Eric Wu <ericwu1989@gmail.com>
2016-08-04 16:00:45 +0800

    Store app name and link into db

--- apple_app_spider.go
+++ apple_app_spider.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/jinzhu/gorm"
@@ -12,28 +13,38 @@ import (
 type App struct {
 	gorm.Model
 
-	Name string
-	Link string
+	Name   string
+	Link   string
+	Letter string
 }
 
 func Spider() {
-	doc, err := goquery.NewDocument("https://itunes.apple.com/cn/genre/ios-tu-shu/id6018?mt=8&letter=A&page=5")
+	db, err := gorm.Open("mysql", "root:@/applist?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	doc.Find("#selectedcontent .column ul li").Each(func(i int, s *goquery.Selection) {
-		band := s.Find("a").Text()
-		fmt.Printf("%d: %s\n", i, band)
-	})
+	url := "https://itunes.apple.com/cn/genre/ios-tu-shu/id6018?mt=8&letter=A&page="
+
+	for i := 1; i <= 29; i++ {
+		doc, err := goquery.NewDocument(url + strconv.Itoa(i))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		doc.Find("#selectedcontent .column ul li").Each(func(i int, s *goquery.Selection) {
+			target := s.Find("a")
+			band := target.Text()
+			link, _ := target.Attr("href")
+
+			app := App{Name: band, Link: link, Letter: "A"}
+			db.Create(&app)
+
+			fmt.Printf("%d: %s-%s\n", i, band, link)
+		})
+	}
 }
 
 func main() {
-	db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	Spider()
 }
